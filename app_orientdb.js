d3c370b29bab04ffdb3978cbcc4e04578d04b391
Juil Kim <iceship@gmail.com>
2017-05-07 23:56:52 +1000

    orient db

--- app_orientdb.js
+++ app_orientdb.js
@@ -20,37 +20,64 @@ var db = server.use('o2')
 app.set('views', './views_orientdb');
 app.set('view engine', 'pug');
 
-app.get('/topic/new', function(req, res){
-  fs.readdir('data', function(err, files){
-    if(err){
-      console.log(err);
-      res.status(500).send('Internal Server Error');
-    }
-    res.render('new', {topics:files});
+app.get('/topic/add', function(req, res){
+    var sql = 'select from topic';
+    db.query(sql).then(function(topics){
+        if(topics.length === 0){
+            console.log('There is no topic record.');
+            res.status(500).send('Internal Server Error');
+        }
+        res.render('add', {topics:topics});
+    });
+});
+
+app.post('/topic/add', function(req, res){
+  var title = req.body.title;
+  var description = req.body.description;
+  var author = req.body.author;
+  var sql = 'insert into topic (title, description, author) values (:title, :desc, :author)';
+  db.query(sql, {params: { 
+      title:title,
+      desc:description,
+      author:author
+  }}).then(function(records){
+    //console.log(records);
+    res.redirect('/topic/'+ records[0].title);
   });
 });
+
+app.get('/topic/:topic/edit', function(req, res){
+    var topic = req.params.topic;
+    var sql = 'select from topic where title=:topic';
+    db.query(sql, {params:{topic:topic}}).then(function(topics){
+        if(topics.length === 0){
+            console.log('There is no topic record.');
+            res.status(500).send('Internal Server Error');
+        }
+            console.log(topics[0]);
+        res.render('edit', {topic:topics[0]});
+    });
+});
+
 
 app.get(['/topic', '/topic/:topic'], function(req, res){
   var sql = 'select from topic';
   db.query(sql).then(function(records){
-    console.log(records);
-  });
-
-});
-
-app.post('/topic', function(req, res){
-  var title = req.body.title;
-  var description = req.body.description;
-  fs.writeFile('data/'+title, description, function(err){
-    if(err){
-      console.log(err);
-      res.status(500).send('Internal Server Error');
-    }
-    //res.send('Success');
-    res.redirect('/topic/'+title);
-
+    var topic = req.params.topic;
+    if(topic){
+      var sql = 'select from topic where title=:title';
+      db.query(sql, {params:{title:topic}}).then(function(record){
+        
+        res.render('view', {topics:records,topic:record[0]});
+      });
+    } else {
+        //var topic = {title:"Hello", description:"World!"};
+        res.render('view', {topics:records}); //,topic:topic});
+    };
   });
 });
+
+
 
 app.listen(3000, function(){
     console.log('Connected, 3000 port')
