c6919a8fe4837b12a71129a363f43d6ac6a8f02f
Juil Kim <iceship@gmail.com>
2017-05-07 12:48:00 +1000

    file save and read

---
+++ app_file.js
@@ -0,0 +1,61 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var fs = require('fs');
+
+var app = express();
+app.locals.pretty = true;
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.set('views', './views_file');
+app.set('view engine', 'pug');
+
+app.get('/topic/new', function(req, res){
+  fs.readdir('data', function(err, files){
+    if(err){
+      console.log(err);
+      res.status(500).send('Internal Server Error');
+    }
+    res.render('new', {topics:files});
+  });
+});
+
+app.get(['/topic', '/topic/:topic'], function(req, res){
+  fs.readdir('data', function(err, files){
+    if(err){
+      console.log(err);
+      res.status(500).send('Internal Server Error');
+    }
+    var id = req.params.topic;
+
+    if(id){
+        fs.readFile('data/'+id, 'utf8', function(err, data){
+          if(err){
+            console.log(err);
+            res.status(500).send('Internal Server Error');
+          };
+          res.render('view', {topics:files, title:id, description:data});
+        });
+    } else {
+        res.render('view', {topics: files, title:'Welcome', description:'Hello JavaScript for Server.'});
+    }
+  });
+
+});
+
+app.post('/topic', function(req, res){
+  var title = req.body.title;
+  var description = req.body.description;
+  fs.writeFile('data/'+title, description, function(err){
+    if(err){
+      console.log(err);
+      res.status(500).send('Internal Server Error');
+    }
+    //res.send('Success');
+    res.redirect('/topic/'+title);
+    
+  });
+});
+
+app.listen(3000, function(){
+    console.log('Connected, 3000 port')
+});
